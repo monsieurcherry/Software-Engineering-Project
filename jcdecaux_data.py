b96de3ff9c43bca3c4d4df5ab237bbf2303aa8d0
Itgel Ganbold <itgel98@gmail.com>
2023-02-21 10:25:10 +0000

    Scrape every 5min

--- jcdecaux_data.py
+++ jcdecaux_data.py
@@ -1,6 +1,7 @@
 import requests
-# import dbinfo
+#import dbinfo
 import json
+import time
 
 
 api_key = 'a471198f1d4a279171da8f17892b64eb12c32f33'
@@ -9,5 +10,16 @@ api_query = 'https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey=a47
 r = requests.get(api_query)
 data = (json.loads(r.text))
 
-with open('json_data.json', 'w') as outfile:
-    json.dump(data, outfile)
+
+
+def main():
+    while True:
+        try:
+            r = requests.get(api_query)
+            with open('json_data.json', 'w') as outfile:
+                json.dump(data, outfile)
+            time.sleep(5*60)
+        except:
+            print("Error. Something went wrong.")
+    return 
+main()
