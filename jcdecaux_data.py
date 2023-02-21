8a18bf5961cd470e3ef9c55911df5623011f245f
Itgel Ganbold <itgel98@gmail.com>
2023-02-21 10:03:35 +0000

    now we can store the json

--- jcdecaux_data.py
+++ jcdecaux_data.py
@@ -7,5 +7,7 @@ api_key = 'a471198f1d4a279171da8f17892b64eb12c32f33'
 contract_name = 'dublin'
 api_query = 'https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey=a471198f1d4a279171da8f17892b64eb12c32f33'
 r = requests.get(api_query)
-print(json.loads(r.content))
+data = (json.loads(r.text))
 
+with open('json_data.json', 'w') as outfile:
+    json.dump(data, outfile)
