b6f4d9cfba63d60ab7c60cb2f8917a4ca44ff1f6
Itgel Ganbold <itgel98@gmail.com>
2023-02-27 22:20:24 +0000

    updated scraper

--- get_jcdecaux_to_rds.py
+++ get_jcdecaux_to_rds.py
@@ -1,13 +1,9 @@
 import sqlalchemy as sqla
 from sqlalchemy import create_engine
-import traceback
-import glob
-import os
 from pprint import pprint
 import simplejson as json
 import requests
 from IPython.display import display
-import time
 
 URI = "dbbikes.cfjfzkae45jy.eu-west-1.rds.amazonaws.com"
 PORT="3306"
@@ -17,59 +13,36 @@ PASSWORD = "mypassword"
 
 engine = create_engine("mysql+mysqldb://{}:{}@{}:{}/{}".format(USER, PASSWORD, URI, PORT, DB), echo=True)
 
-
 sql = """
 CREATE DATABASE IF NOT EXISTS dbbikes;
 """
-
 engine.execute(sql)
 
-sql = """
-CREATE TABLE IF NOT EXISTS station (
-address VARCHAR(256),
-banking INTEGER,
-bike_stands INTEGER,
-bonus INTEGER,
-contract_name VARCHAR(256),
-name VARCHAR(256),
-number INTEGER,
-position_lat REAL,
-position_lng REAL,
-status VARCHAR(256)
-)
-"""
-
-try:
-    res = engine.execute("DROP TABLE IF EXISTS station")
-    res = engine.execute(sql)
-    print(res.fetchall())
-except Exception as e:
-    print(e)
-
-
-
-
-sql = """
-CREATE TABLE IF NOT EXISTS availability(
-number INTEGER,
-available_bikes INTEGER,
-available_bike_stands INTEGER,
-last_update INTEGER
-)
-"""
-
-try:
-    res = engine.execute("DROP TABLE IF EXISTS availability")
-    res = engine.execute(sql)
-    print(res.fetchall())
-except Exception as e:
-    print(e)
-
 def station_to_db(text):
+    sql = """
+    CREATE TABLE IF NOT EXISTS station (
+    address VARCHAR(256),
+    banking INTEGER,
+    bike_stands INTEGER,
+    bonus INTEGER,
+    contract_name VARCHAR(256),
+    name VARCHAR(256),
+    number INTEGER,
+    position_lat REAL,
+    position_lng REAL,
+    status VARCHAR(256)
+    )
+    """
+
+    try:
+        res = engine.execute("DROP TABLE IF EXISTS station")
+        res = engine.execute(sql)
+    except Exception as e:
+        print(e)
+    
+    
     stations = json.loads(text)
-    print(type(stations), len(stations))
     for station in stations:
-        print(station)
         vals = (station.get('address'),
                 int(station.get('banking')),
                 station.get('bike_stands'),
@@ -81,14 +54,29 @@ def station_to_db(text):
                 station.get('position').get('lng'),
                 station.get('status'))
         engine.execute("insert into station values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)", vals)
-        break #Because of this break, the loop only inserts the first Station. Which is no.42.
     return
 
+
+
+
 def availability_to_db(text):
+    sql = """
+    CREATE TABLE IF NOT EXISTS availability(
+    number INTEGER,
+    available_bikes INTEGER,
+    available_bike_stands INTEGER,
+    last_update INTEGER
+    )
+    """
+
+    try:
+        res = engine.execute("DROP TABLE IF EXISTS availability")
+        res = engine.execute(sql)
+    except Exception as e:
+        print(e)
+        
     stations = json.loads(text)
-    print(type(stations), len(stations))
     for station in stations:
-        print(station)
         vals = (int(station.get('number')),
                 int(station.get('available_bikes')),
                 int(station.get('available_bike_stands')),
@@ -97,11 +85,6 @@ def availability_to_db(text):
         engine.execute("insert into availability values(%s,%s,%s,%s)", vals)
     return
 
-def stations_fix_keys(station):
-    station['position_lat'] = station['position']['lat']
-    station['position_lng'] = station['position']['lng']
-    return station
-
 
 
 
@@ -109,23 +92,18 @@ api_key = 'a471198f1d4a279171da8f17892b64eb12c32f33'
 contract_name = 'dublin'
 api_query = 'https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey=a471198f1d4a279171da8f17892b64eb12c32f33'
 
-
-
+import time
 
 def main():
     while True:
-        
-        global r
         try:
-            r = requests.get('https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey=a471198f1d4a279171da8f17892b64eb12c32f33')
+            r = requests.get(api_query)
             station_to_db(r.text)
             availability_to_db(r.text)
-            stations = json.loads(r.text)
-            # engine.execute(station.insert(), *map(stations_fix_keys, stations))
-
+            print("scraping in progress")
             time.sleep(5*60) #Scrape every 5 minutes
         except:
             print("Error. Something went wrong.") 
-    
-main()
+    return
 
+main()
