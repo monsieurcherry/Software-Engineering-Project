9e1eed5111bb8097008dcaa2b0ab2973b16bc93c
Itgel Ganbold <itgel98@gmail.com>
2023-03-02 15:30:30 +0000

    ..

--- Web Weather Scraper/open_weather_scraper.py
+++ Web Weather Scraper/open_weather_scraper.py
@@ -68,7 +68,7 @@ def main():
             weather_to_db(weather_r.text)
             print("Weather scraping is done, now waiting...")
             connection.close()
-            time.sleep(300) #Scrape every 5 minutes
+            time.sleep(60) #Scrape every 5 minutes
         except:
             print("Error. Something went wrong.") 
     return
