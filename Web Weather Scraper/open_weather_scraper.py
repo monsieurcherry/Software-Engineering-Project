2b7ac69c02c84069a4cdb8dcdb1d5968065359ba
Itgel Ganbold <itgel98@gmail.com>
2023-03-02 10:31:14 +0000

    lets test our weather

--- Web Weather Scraper/open_weather_scraper.py
+++ Web Weather Scraper/open_weather_scraper.py
@@ -1,12 +1,68 @@
 import requests
 import json
+import sqlalchemy as sqla
+from sqlalchemy import create_engine
+from pprint import pprint
+import simplejson as json
+import requests
+from IPython.display import display
+import datetime
+import time
+
+URI = "dbbikes.cfjfzkae45jy.eu-west-1.rds.amazonaws.com"
+PORT="3306"
+DB="dbbikes"
+USER="admin"
+PASSWORD = "mypassword"
+
+engine = create_engine("mysql+mysqldb://{}:{}@{}:{}/{}".format(USER, PASSWORD, URI, PORT, DB), echo=True)
+
+sql = """
+CREATE DATABASE IF NOT EXISTS dbbikes;
+"""
+engine.execute(sql)
+
+def weather_to_db(text):
+    sql = """
+    CREATE TABLE IF NOT EXISTS weather (
+    time DATETIME,
+    temperature INTEGER,
+    windspeed INTEGER,
+    pressure INTEGER,
+    description VARCHAR(256),
+    cloudiness VARCHAR(256)
+    )
+    """
+
+    res = engine.execute(sql)
+        
+    
+    weather_infos = json.loads(text)
+    vals = (datetime.datetime.fromtimestamp(int(weather_infos.get('dt'))),
+            int(weather_infos.get('main').get('temp')),
+            weather_infos.get('wind').get('speed'),
+            int(weather_infos.get('main').get('pressure')),
+            weather_infos.get('weather').get('0').get('description'),
+            weather_infos.get('clouds').get('all'))
+    engine.execute("insert into station values(%s,%s,%s,%s,%s,%s)", vals)
+    return
+
 
 api_key = 'b7d6a55bc0fff59fb0d5f7c3c1668417'
 lat='53.35'
 lon='-6.26'
 city = 'dublin'
-api_query = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units=metric"
+weather_api_query = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units=metric"
 
+def main():
+    while True:
+        try:
+            weather_r = requests.get(weather_api_query)
+            weather_to_db(weather_r.text)
+            print("Weather scraping is done, now waiting...")
+            time.sleep(61) #Scrape every 5 minutes
+        except:
+            print("Error. Something went wrong.") 
+    return
 
-r = requests.get(api_query)
-print(json.loads(r.text))
+main()
